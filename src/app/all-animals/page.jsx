2dcef02221b636029d559032a18ed5816303fdcd
Mehedi-tsc <mehedimmc1@gmail.com>
2026-05-01 06:33:51 +0600

    all animals section without sorting is added

--- src/app/all-animals/page.jsx
+++ src/app/all-animals/page.jsx
@@ -1,9 +1,20 @@
+import AnimalCard from '@/ui/AnimalCard';
 import React from 'react';
 
-const AllAnimalPage = () => {
+const AllAnimalPage = async () => {
+    const res = await fetch('https://my-qurbani-hat.vercel.app/data.json')
+    const animals = await res.json()
     return (
-        <div>
-            All animals
+        <div className='my-5 w-11/12 lg:max-w-7xl mx-auto'>
+            <div className='mb-3 flex flex-col md:flex-row lg:flex-row md:justify-between lg:justify-between'>
+                <h2 className=' text-3xl font-bold'>All Animals</h2>
+                
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+                {
+                    animals.map(animal => <AnimalCard key={animal.id} animal={animal}></AnimalCard>)
+                }
+            </div>
         </div>
     );
 };
